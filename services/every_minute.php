8f21a5df99912782c419918138ce6423dfb25e5d
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-05-24 19:17:29 -0600

    adding git pull script

---
+++ services/every_minute.php
@@ -0,0 +1,5 @@
+<?php
+//if(!defined('main_already_included'))require_once("../../../includes/main.php");
+echo "pull remote sensors";
+PullRemoteSensors::Sync();
+?>
--- services/sync_remote.php
+++
@@ -1,6 +0,0 @@
-<pre><?php
-require_once("../../../includes/main.php");
-echo "hello ";
-PullRemoteSensors::Sync();
-echo "world?";
-?></pre>
