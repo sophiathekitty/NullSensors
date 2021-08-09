10676c34a6367229f4032caeff98ef99b1528470
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-08-09 16:47:37 -0600

    spell temperature right for api path

--- api/temperature/index.php
+++ api/temperature/index.php
@@ -7,6 +7,8 @@ if(isset($_GET['gpio'],$_GET['error'])){
 } else if(isset($_GET['gpio'],$_GET['temperature'],$_GET['humidity'])){
     // report local temperature sensor data
     TemperatureLogger::RecordTemperature($_GET['gpio'],$_GET['temperature'],$_GET['humidity']);
+} else if(isset($_GET['id'])){
+    $data['sensor'] = TemperatureSensors::LoadSensorId($_GET['id']);
 } else if(isset($_GET['working'])){
     $data['temperature'] = TemperatureSensors::LoadWorkingSensors();
 } else if(isset($_GET['average'])){
--- python/piSensors.py
+++ python/piSensors.py
@@ -17,14 +17,14 @@ while True:
         print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
             .format(temperature_f, temperature_c, humidity))
         error_count = 0
-        with urllib.request.urlopen("http://localhost/plugins/NullSensors/api/tempature?gpio=17&temperature={}&humidity={}".format(temperature_f,humidity)) as json_url:
+        with urllib.request.urlopen("http://localhost/plugins/NullSensors/api/temperature?gpio=17&temperature={}&humidity={}".format(temperature_f,humidity)) as json_url:
             buf = json_url.read()
             data = json.loads(buf.decode('utf-8'))
     except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
         error_count = error_count + 1
         if(error_count > 100):
-            with urllib.request.urlopen("http://localhost/plugins/NullSensors/api/tempature?gpio=17&error={}".format(error.args[0])) as json_url:
+            with urllib.request.urlopen("http://localhost/plugins/NullSensors/api/temperature?gpio=17&error={}".format(error.args[0])) as json_url:
                 buf = json_url.read()
                 data = json.loads(buf.decode('utf-8'))
             
