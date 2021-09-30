dcf1db165c8785b1529dfb52b8a181173557037e
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-09-30 16:11:47 -0600

    i guess i should commit this

--- api/temperature/index.php
+++ api/temperature/index.php
@@ -1,6 +1,7 @@
 <?php
 require_once("../../../../includes/main.php");
 $data = [];
+$data['get'] = $_GET;
 if(isset($_GET['gpio'],$_GET['error'])){
     // report error state
     TemperatureLogger::RecordError($_GET['gpio'],$_GET['error']);
--- api/temperature/room/index.php
+++ api/temperature/room/index.php
@@ -1,6 +1,12 @@
 <?php
 require_once("../../../../../includes/main.php");
 $data = [];
-$data['room'] = RoomTemperature();
+if(isset($_GET['room_id'])){
+    $data['room'] = RoomDHT11::RoomTemperature($_GET['room_id']);
+} else if(isset($_GET['average'])){
+    $data['room'] = AverageIndoorTemperature();
+} else {
+    $data['room'] = RoomTemperature();
+}
 OutputJson($data);
 ?>
