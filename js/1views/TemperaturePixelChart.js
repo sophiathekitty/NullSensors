18ecb86fc442180668c549da41a36da7eba0df33
sophia daniels <sophiathekitty@users.noreply.github.com>
2022-02-10 00:21:44 -0700

    indoor pixel chart stuff for weather stamp #1

--- js/1views/TemperaturePixelChart.js
+++ js/1views/TemperaturePixelChart.js
@@ -1,21 +1,60 @@
 class TemperaturePixelChart extends View {
     constructor(debug = IndoorTemperatureHourlyChart.debug_temperature){
         super(IndoorTemperatureHourlyChart.instance,
-            new Template("dht11_item","/plugins/NullSensors/templates/charts/pixel.html"),
+            new Template("indoor_pixel","/plugins/NullSensors/templates/charts/pixel.html"),
             null,
             90000,debug);
             this.pallet = ColorPallet.getPallet("weather");
     }
-    build(){
-        if(this.debug) console.warn("TemperaturePixelChart::Build","missing room id");
+    buildWeather(){
+        if(this.debug) console.log("TemperaturePixelChart::Build",$("#indoors_weather_stamp").length);
+        if($("#indoors_weather_stamp").length > 0){
+            if(this.template){
+                this.template.getData(html=>{
+                    if($("#indoors_weather_stamp .temp_chart.indoors.simple").length == 0) $(html).appendTo("#indoors_weather_stamp");
+                    this.displayWeather();
+                });
+            }
+        }
     }
-    refresh(){
-        if(this.debug) console.warn("TemperaturePixelChart::Refresh","missing room id");
+    refreshWeather(){
+        if(this.debug) console.log("TemperaturePixelChart::Refresh",$("#indoors_weather_stamp .temp_chart.indoors.simple").length);
+        if($("#indoors_weather_stamp .temp_chart.indoors.simple").length == 0){
+            this.buildWeather();
+        } else {
+            this.displayWeather();
+        }
     }
-    display(){
-        if(this.debug) console.warn("TemperaturePixelChart::Display","missing room id");
+    displayWeather(){
+        if(this.debug) console.log("TemperaturePixelChart::Display");
+        if(this.model){
+            this.model.getData(json=>{
+                if(this.debug) console.log("TemperaturePixelChart::Display-json",json);
+                json.temperature.forEach(hour=>{
+                    if(this.debug) console.log("TemperaturePixelChart::Display-hour",hour);
+                    this.pallet.getColorLerp("temp",hour.temp,color=>{
+                        var hours = Number(hour.hour);
+                        var am = "am";
+                        if(hours > 12){
+                            am = "pm";
+                            hours -= 12;
+                        }
+                        if(hours == 12){
+                            am = "pm";
+                        }
+                        if(hours == 0){
+                            hours = 12;
+                        }
+                        $("#indoors_weather_stamp .temp_chart.indoors.simple [hour="+hour.hour+"]").css("background-color",color);
+                        $("#indoors_weather_stamp .temp_chart.indoors.simple [hour="+hour.hour+"]").attr("title","Indoors -- "+hours+am+"\nTemp: "+Math.round(hour.temp)+"° | "+Math.round(hour.temp_max)+"° / "+Math.round(hour.temp_min)+"°\nHum: "+Math.round(hour.hum)+"% | "+Math.round(hour.hum_max)+"% / "+Math.round(hour.hum_min)+"%");
+                    });
+
+                });
+            });
+        }
     }
     build(room_id){
+        //if(this.debug) console.log("TemperaturePixelChart::Build(room_id)",room_id);
         if(this.template){
             this.template.getData(html=>{
                 this.model.room(room_id,json=>{
@@ -26,9 +65,11 @@ class TemperaturePixelChart extends View {
         }
     }
     refresh(room_id){
+        //if(this.debug) console.log("TemperaturePixelChart::Refresh(room_id)",room_id);
         this.display(room_id);
     }
     display(room_id){
+        //if(this.debug) console.log("TemperaturePixelChart::Display(room_id)",room_id);
         if(this.model){
             this.model.room(room_id,json=>{
                 json.temperature.forEach(hour=>{
